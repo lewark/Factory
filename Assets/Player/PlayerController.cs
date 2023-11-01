4f1752472028b7955dd90cb82e520f9eb0a3b1cd
lewark <78086132+lewark@users.noreply.github.com>
2023-11-01 10:23:54 -0600

    Improve hair, add base movement code

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController controller;
+    Animator animator;
+
+    public GameObject viewCamera;
+    public float moveSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        controller.Move();
+        Vector3 forward = viewCamera.transform.forward;
+        Vector3 right = viewCamera.transform.right;
+
+        Vector3 moveDir = Input.GetAxisRaw("Vertical") * forward + Input.GetAxisRaw("Horizontal") * right;
+        moveDir.y = 0;
+        moveDir = moveDir.normalized;
+
+        animator.SetBool("isWalking", moveDir.magnitude > 0.01);
+
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
     }
 }
