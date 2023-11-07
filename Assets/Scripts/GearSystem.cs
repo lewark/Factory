69adbc7ab755767607f59c609b0853c560e752da
lewark <78086132+lewark@users.noreply.github.com>
2023-11-07 00:50:55 -0700

    Add gear animations

---
+++ Assets/Scripts/GearSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearSystem : MonoBehaviour
+{
+    public float speed = 0;
+    public float sineAmplitude = 0;
+    public float sineSpeed = 0;
+    public float sinePhase = 0;
+    public GameObject anchor = null;
+    public GameObject[] forwardGears;
+    public GameObject[] reverseGears;
+
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        float rotation = time * speed + Mathf.Sin(time * sineSpeed + sinePhase) * sineAmplitude;
+
+        Quaternion angle = Quaternion.Euler(0, 0, rotation);
+        transform.rotation = angle;
+        foreach (GameObject obj in forwardGears)
+        {
+            obj.transform.rotation = angle;
+        }
+
+        angle = Quaternion.Euler(0, 0, -rotation);
+        foreach (GameObject obj in reverseGears)
+        {
+            obj.transform.rotation = angle;
+        }
+    }
+}
